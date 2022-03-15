2e6dae7b90930de0e8ecd136d2a8032487bb936c
M-A-T-T-11 <m4tt_0@icloud.com>
2022-03-14 20:17:49 -0400

    Removed old GUI added new ones

--- SignInApp.java
+++ SignInApp.java
@@ -1,36 +1,106 @@
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.ArrayList;
+import java.awt.Font;
 
 public class SignInApp { 
 
     private ArrayList<User> users;
+    private JFrame app;
+    private JPanel topPanel;
+    private JPanel bodyPanel;
+    private JPanel bottomPanel;
+    private String[] strNames;
 
     public SignInApp() {
         users = new ArrayList<User>();
-        String[] strNames = getNames();
+        strNames = getNames();
+        app = new JFrame();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setTitle("Sign-In Sheet");
+        app.setBounds(100, 100, 500, 300);
+        app.setLayout(new BorderLayout());
+        app.setResizable(false);
 
-        JFrame frame = new JFrame();
-        frame.setLayout(new GridLayout(5, 1));
-        frame.setTitle("Sign-In Sheet");
-        
-        JComboBox names = new JComboBox(strNames);
-        JButton signIn = new JButton("Sign In");
+        createElements();
+        app.add(topPanel, BorderLayout.NORTH);
+        app.add(bodyPanel, BorderLayout.CENTER);
+        app.add(bottomPanel, BorderLayout.SOUTH);
+        app.setVisible(true);
+    }
+
+    private void createElements() {
+        // Create header -> Sign-in text
+        topPanel = new JPanel();
+        topPanel.setBorder(BorderFactory.createEmptyBorder(15, 25, 5, 25));
+        topPanel.setBackground(Color.WHITE);
+
+        JLabel title = new JLabel("Sign-in Sheet", SwingConstants.CENTER);
+        title.setFont(new Font("Helvetica", Font.BOLD, 24));
+        topPanel.add(title);
+
+        // Create body -> everything else
+        bodyPanel = new JPanel();
+        bodyPanel.setBorder(BorderFactory.createEmptyBorder(5, 30, 5, 31));
+        bodyPanel.setLayout(new GridLayout(3, 1, 0, 5));
+        bodyPanel.setBackground(Color.WHITE);
+
+        JComboBox<String> namesComboBox = new JComboBox<String>(strNames);
+        namesComboBox.setPreferredSize(new Dimension(210, 20));
+        namesComboBox.setBackground(Color.WHITE);
+
+        JButton signIn = new JButton("Sign  In  ");
+        signIn.setPreferredSize(new Dimension(210, 20));
+        signIn.setFont(new Font("Helvetica", Font.BOLD, 16));
+        signIn.setBackground(Color.WHITE);
+        signIn.addActionListener(e -> {
+
+        });
+
         JButton signOut = new JButton("Sign Out");
+        signOut.setPreferredSize(new Dimension(210, 20));
+        signOut.setFont(new Font("Helvetica", Font.BOLD, 16));
+        signOut.setBackground(Color.WHITE);
+        signOut.addActionListener(e -> {
+
+        });
+
+        bodyPanel.add(namesComboBox);
+        bodyPanel.add(signIn);
+        bodyPanel.add(signOut);
+
+        // Create footer -> addUser and removeUser buttons
+        bottomPanel = new JPanel();
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(5, 25, 15, 25));
+        bottomPanel.setBackground(Color.WHITE);
+
         JButton addUser = new JButton("Add User");
+        addUser.setPreferredSize(new Dimension(210, 50));
+        addUser.setFont(new Font("Helvetica", Font.BOLD, 18));
+        addUser.setBackground(Color.WHITE);
+        addUser.addActionListener(e -> {
+            
+        });
+
         JButton removeUser = new JButton("Remove User");
+        removeUser.setPreferredSize(new Dimension(210, 50));
+        removeUser.setFont(new Font("Helvetica", Font.BOLD, 18));
+        removeUser.setBackground(Color.WHITE);
+        removeUser.addActionListener(e -> {
+            
+        });
 
-        frame.add(names);
-        frame.add(signIn);
-        frame.add(signOut);
-        frame.add(addUser);
-        frame.add(removeUser);
-
-        frame.setSize(400, 400);
-        frame.setVisible(true);
-        
+        bottomPanel.add(addUser);
+        bottomPanel.add(removeUser);
     }
 
     private String[] getNames() {
